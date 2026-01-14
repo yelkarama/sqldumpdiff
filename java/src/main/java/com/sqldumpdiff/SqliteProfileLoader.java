332cd112b95e4592cd7f5eba5c29ac6745e974a9
Younes El karama <yelkarama@gmail.com>
2026-01-14 17:19:16 -0500

    fix: Enhance YAML loading with LoaderOptions in SqliteProfileLoader

--- java/src/main/java/com/sqldumpdiff/SqliteProfileLoader.java
+++ java/src/main/java/com/sqldumpdiff/SqliteProfileLoader.java
@@ -2,6 +2,7 @@ package com.sqldumpdiff;
 
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.LoaderOptions;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,7 +12,7 @@ import java.nio.file.Path;
 public class SqliteProfileLoader {
     public static SqliteProfilesFile load(Path path) throws IOException {
         try (InputStream in = Files.newInputStream(path)) {
-            Yaml yaml = new Yaml(new Constructor(SqliteProfilesFile.class));
+            Yaml yaml = new Yaml(new Constructor(SqliteProfilesFile.class, new LoaderOptions()));
             return yaml.load(in);
         }
     }
